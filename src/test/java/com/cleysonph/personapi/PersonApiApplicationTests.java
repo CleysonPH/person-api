57699149ee8681eb0d3ee4acdda5e3436e6652c6
Cleyson Lima <cleysonph@gmail.com>
2021-03-31 16:23:06 -0300

    initial commit

---
+++ src/test/java/com/cleysonph/personapi/PersonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleysonph.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
